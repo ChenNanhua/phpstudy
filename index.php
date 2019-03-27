2e87f1dd85a0270b45e235e3d6eb01dfd6dda56b
ReinholdChen <2630966248@qq.com>
2019-03-27 22:01:57 +0800

    部分修订

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
     <link rel="icon" href="favicon.ico" type="image/x-icon" />
+    <like href="D:\phpStudy\PHPTutorial\WWW\mystyle.css" type="text/css" rel="stylesheet"/>
     <title>$_POST获取表单post信息</title>
 </head>
 <body>
